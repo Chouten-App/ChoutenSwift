9b5d0e50a6afcceb11723b650065519e725db0db
LawlietAnime <ryusakilawlietgithub@gmail.com>
2023-03-18 20:39:55 +0100

    Initial Commit

---
+++ ModularSaikouS/ModularSaikouSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularSaikouSApp.swift
+//  ModularSaikouS
+//
+//  Created by Inumaki on 18.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ModularSaikouSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
